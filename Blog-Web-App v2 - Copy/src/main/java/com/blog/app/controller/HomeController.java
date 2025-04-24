1d44b0c9a35da0f6f1c9790d7148b1bba8d137f9
Pravin Kumar <115208590+Pravinkumar295@users.noreply.github.com>
2025-04-24 23:37:04 +0530

    Update HomeController.java added check request

--- Blog-Web-App v2 - Copy/src/main/java/com/blog/app/controller/HomeController.java
+++ Blog-Web-App v2 - Copy/src/main/java/com/blog/app/controller/HomeController.java
@@ -7,10 +7,12 @@ import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.blog.app.models.Post;
 import com.blog.app.service.PostService;
@@ -46,5 +48,12 @@ public class HomeController {
 		model.addAttribute("posts", posts_on_page);
 		return "home_views/home"; 
 	} 
-	
+	@RestController
+	public class HealthController {
+
+	    @GetMapping("/health")
+	    public ResponseEntity<String> health() {
+	        return ResponseEntity.ok("OK");
+	    }
+	}
 }
