597182b7dc914e96d37486a139e3929b15f4e156
Pravin Kumar <115208590+Pravinkumar295@users.noreply.github.com>
2025-04-25 00:03:52 +0530

    Update HomeController.java

--- Blog-Web-App v2 - Copy/src/main/java/com/blog/app/controller/HomeController.java
+++ Blog-Web-App v2 - Copy/src/main/java/com/blog/app/controller/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.blog.app.models.Post;
 import com.blog.app.service.PostService;
@@ -23,6 +24,7 @@ public class HomeController {
 	private PostService postService;
 
 	@GetMapping("/")
+	@ResponseBody
 		public String newHoome() {
 			return "Hello World";
 		}
